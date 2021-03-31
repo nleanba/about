6e6278196241111bdc4609addfc85c094e885885
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-31 21:04:07 +0000

    Deploy nleanba/about to github.com/nleanba/about.git:gh-pages

---
+++ precache-manifest.2a1302a3ae7a22b707c03147dae8d0b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0438fdad9b4c2a5ae0233250f5011d",
+    "url": "/192.png"
+  },
+  {
+    "revision": "28e6ec8c7abe3acf1dfc7e6c3b7acfe7",
+    "url": "/512.png"
+  },
+  {
+    "revision": "b174d187478a70d9343df46e0489a33d",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "6d3211ea88e9bbc93a61",
+    "url": "/css/app.a302f736.css"
+  },
+  {
+    "revision": "b8e9682d086b8c0fa03766c459ac719a",
+    "url": "/icon.svg"
+  },
+  {
+    "revision": "4ab2119219603f0d971efc724dad2aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d3211ea88e9bbc93a61",
+    "url": "/js/app.6722d0a3.js"
+  },
+  {
+    "revision": "24783cb6b94e2cfd1f09",
+    "url": "/js/chunk-vendors.e70b44fe.js"
+  },
+  {
+    "revision": "5f513f2d6aa61bf62b3719ea21a3c746",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "31b508128326a32c46868a3835cd9460",
+    "url": "/manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
